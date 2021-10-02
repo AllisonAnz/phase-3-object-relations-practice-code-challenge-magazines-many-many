016d5a543e35a62896cb25278664dd75e28436e8
AllisonAnz <AllisonAnzalone@gmail.com>
2021-10-02 14:30:21 -0500

    Completed

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,19 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  # Magazine#email_list 
+  # return a string of a semi-colon separated list of emails from all the readers subscribed to this magazine 
+  # email1@example.com;email2@example.com;email3@example.com
+  def email_list 
+    self.readers.map do |reader|
+        "#{reader.email}; "
+    end
+  end
+
+  # Magazine.most_populate 
+  # returns the Magazine instance with the most subscriptions 
+  def self.most_popular 
+    self.all.max_by {|magazine| magazine.subscriptions.length}
+  end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,28 @@
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  # Reader#subscribe(magazine, price)
+  # takes a magazine (an instance of the Magazine class) and price (integer) as arguments 
+  # and creates a new subscription in the database associated with the magazine and the reader 
+  def subscribe(magazine, price)
+    Subscription.create(price: price, magazine_id: magazine.id, reader_id: self.id)
+  end
+
+  # Reader#total_subscription_price 
+  # returns total price for all the reader's subscriptions 
+  def total_subscription_price
+    sum = 0
+    self.subscriptions.each do |subscription|
+        sum += subscription.price 
+    end
+    sum
+  end
+
+  # Reader#cancel_subscription(magazine)
+  # takes a magazine instance and removes the subscription for this reader 
+  # you will have to delete a row from the subscription table for this to work 
+  def cancel_subscription(magazine)
+    self.subscriptions.find {|subscription| subscription.magazine  === magazine}.delete
+  end
 end
